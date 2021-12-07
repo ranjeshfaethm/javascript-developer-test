3eb7a7485293a6dbed47c4a2586e879b2c65b13b
cranjesh <63446800+cranjesh@users.noreply.github.com>
2021-12-07 20:37:45 +1100

    Update get-arnie-quotes.js

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,12 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const promises = urls.map(async url => (await httpGet(url)));
+  return (await Promise.allSettled(promises)).map(result => {
+    const { status, body } = result.value;
+    if (status === 200) return { 'Arnie Quote': JSON.parse(body)['message'] };
+    else return { 'FAILURE': JSON.parse(body)['message'] };
+  });
 };
 
 module.exports = {
